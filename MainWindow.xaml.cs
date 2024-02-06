c20496fe509e7ed37443815482f676c457c3327e
16semka19 <fullfocussemka@gmail.com>
2024-02-06 15:44:23 +0200

    Complete task

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows;
+﻿using System.Linq;
+using System.Windows;
 using System.Windows.Controls;
 
 namespace TodoApp
@@ -13,12 +14,23 @@ namespace TodoApp
             InitializeComponent();
         }
 
+        private void CheckBox_Checked(object sender, RoutedEventArgs e)
+        {
+            bool allTasksCompleted = tasksPanel.Children.OfType<CheckBox>().All(c => c.IsChecked == true);
+
+            if (allTasksCompleted)
+            {
+                MessageBox.Show("Ти - молодець! Всі завдання виконані, так тримати!");
+            }
+        }
+
         private void addTaskButton_Click(object sender, RoutedEventArgs e)
         {
             var todoItem = new CheckBox();
             todoItem.Content = taskInput.Text;
             todoItem.Width = 200;
             todoItem.Height = 24;
+            todoItem.Checked += CheckBox_Checked;
             tasksPanel.Children.Add(todoItem);
             taskInput.Clear();
 
