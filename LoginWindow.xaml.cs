9051a29446e9f8162a383197e1356f87fbdd1702
Maksym Voronov <mvoronov.polytech@gmail.com>
2024-02-22 20:24:45 +0200

    Implement placeholder

--- LoginWindow.xaml.cs
+++ LoginWindow.xaml.cs
@@ -26,5 +26,29 @@ namespace TodoApp
             new MainWindow().Show();
             Close();
         }
+
+        private void LoginPlaceholder_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            // оскільки плейсхолдер розташований поверх текстового поля за допомогою властивості Panel.ZIndex="1"
+            // то перехватуємо клік мишки на плейсхолдері та фокусуємо текстове поле
+            LoginTxt.Focus();
+        }
+
+        private void LoginTxt_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            var loginTextIsEmpty = string.IsNullOrWhiteSpace(LoginTxt.Text);
+            // Якщо текст не введено
+            if (loginTextIsEmpty)
+            {
+                // показуємо пейсхолдер "Enter username"
+                this.LoginPlaceholder.Visibility = Visibility.Visible;
+            }
+            else
+            {
+                // скриваємо пейсхолдер "Enter username"
+                this.LoginPlaceholder.Visibility = Visibility.Hidden;
+            }
+        }
     }
+
 }
