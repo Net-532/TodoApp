c8ace49a676d5d110162d2db9c0906692f292e7d
Maksym Voronov <mvoronov.polytech@gmail.com>
2024-03-04 23:17:27 +0200

    Implement DatabaseConnection wrapper as singleton

--- AuthenticationService.cs
+++ AuthenticationService.cs
@@ -9,29 +9,26 @@ namespace TodoApp
     {
         public void Authenticate(string login, string password)
         {
-            string connenctionString = @"Data Source=(localdb)\MSSQLLocalDB; Initial Catalog=todoapp;Integrated Security=SSPI;";
-            using (DbConnection conn = new SqlConnection(connenctionString))
+            var userCount = 0;
+            try
             {
+                DbConnection conn = DatabaseConnection.GetInstance().GetConnection();
                 DbCommand dbCommand = new SqlCommand();
                 dbCommand.Connection = conn;
                 dbCommand.CommandText = "SELECT COUNT(*) FROM dbo.users WHERE username=@name AND password=@pass";
                 dbCommand.Parameters.Add(new SqlParameter("@name", login));
                 dbCommand.Parameters.Add(new SqlParameter("@pass", password));
-                var userCount = 0;
-                try
-                {
-                    conn.Open();
-                    userCount = (int)dbCommand.ExecuteScalar();
-                }
-                catch (Exception ex)
-                {
-                    MessageBox.Show(ex.Message);
-                }
-                if (userCount == 0)
-                {
-                    throw new AuthenticationException("Користувача не знайдено або пароль неправильний!");
-                }
+                userCount = (int)dbCommand.ExecuteScalar();
             }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            if (userCount == 0)
+            {
+                throw new AuthenticationException("Користувача не знайдено або пароль неправильний!");
+            }
+
         }
 
     }
---
+++ DatabaseConnection.cs
@@ -0,0 +1,38 @@
+﻿using System.Data.Common;
+using System.Data.SqlClient;
+
+namespace TodoApp
+{
+    internal class DatabaseConnection
+    {
+        private static DatabaseConnection? _instance;
+
+        private readonly string _connenctionString = @"Data Source=(localdb)\MSSQLLocalDB; Initial Catalog=todoapp;Integrated Security=SSPI;";
+
+        private DbConnection connection;
+
+        private DatabaseConnection()
+        {
+            connection = new SqlConnection(_connenctionString);
+        }
+
+        public static DatabaseConnection GetInstance()
+        {
+            if (_instance == null)
+            {
+                _instance = new DatabaseConnection();
+            }
+            return _instance;
+        }
+
+        public DbConnection GetConnection()
+        {
+            if (connection.State == System.Data.ConnectionState.Closed)
+            {
+                connection.Open();
+            }
+            return connection;
+        }
+
+    }
+}
