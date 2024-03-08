94f2f9042f4b91eb56d465fd5ed64af50617ae1c
Maksym Voronov <mvoronov.polytech@gmail.com>
2024-03-09 00:02:25 +0200

    Implement simple users grid

--- DatabaseConnection.cs
+++ DatabaseConnection.cs
@@ -7,13 +7,13 @@ namespace TodoApp
     {
         private static DatabaseConnection? _instance;
 
-        private readonly string _connenctionString = @"Data Source=(localdb)\MSSQLLocalDB; Initial Catalog=todoapp;Integrated Security=SSPI;";
+        public static readonly string ConnenctionString = @"Data Source=(localdb)\MSSQLLocalDB; Initial Catalog=todoapp;Integrated Security=SSPI;";
 
         private DbConnection connection;
 
         private DatabaseConnection()
         {
-            connection = new SqlConnection(_connenctionString);
+            connection = new SqlConnection(ConnenctionString);
         }
 
         public static DatabaseConnection GetInstance()
--- UsersWindow.xaml.cs
+++ UsersWindow.xaml.cs
@@ -1,28 +1,56 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using Microsoft.VisualBasic.Logging;
+using System.Data;
+using System.Data.Common;
+using System.Data.SqlClient;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace TodoApp
 {
-    /// <summary>
-    /// Interaction logic for UsersWindow.xaml
-    /// </summary>
     public partial class UsersWindow : UserControl
     {
+        private SqlDataAdapter _dataAdapter;
+
+        private DataSet _dataSet;
+
         public UsersWindow()
         {
             InitializeComponent();
         }
+
+        private void LoadUsers()
+        {
+            SqlConnection dbConnection = new SqlConnection(DatabaseConnection.ConnenctionString);
+            _dataAdapter = new SqlDataAdapter("SELECT * FROM users", dbConnection);
+            SqlCommand updateCommand = new SqlCommand("UPDATE users SET password=@password WHERE id=@id", dbConnection);
+            updateCommand.Parameters.Add("@username", SqlDbType.NVarChar, 255, "username");
+            updateCommand.Parameters.Add("@password", SqlDbType.Int, 5, "password");
+            updateCommand.Parameters.Add("@id", SqlDbType.Int, 5, "id");
+            _dataAdapter.UpdateCommand = updateCommand;
+
+            SqlCommand insertCommand = new SqlCommand("INSERT INTO users (username, password) VALUES (@username, @password)", dbConnection);
+            insertCommand.Parameters.Add("@username", SqlDbType.NVarChar, 255, "username");
+            insertCommand.Parameters.Add("@password", SqlDbType.NVarChar, 255, "password");
+            _dataAdapter.InsertCommand = insertCommand;
+
+            SqlCommand deleteCommand = new SqlCommand("DELETE FROM users WHERE id=@id", dbConnection);
+            deleteCommand.Parameters.Add("@id", SqlDbType.Int, 5, "id");
+            _dataAdapter.DeleteCommand = deleteCommand;
+
+            _dataSet = new DataSet("usersDs");
+            _dataAdapter.Fill(_dataSet);
+            UsersGrid.ItemsSource = _dataSet.Tables[0].DefaultView;
+        }
+
+        private void WindowLoaded(object sender, System.Windows.RoutedEventArgs e)
+        {
+            LoadUsers();
+        }
+
+        private void SaveButton_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            _dataAdapter.Update(_dataSet);
+        }
     }
+
 }
