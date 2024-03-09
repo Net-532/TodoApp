51d3c61c8155140abf5fe8c127b2cdf608e397d9
Maksym Voronov <mvoronov.polytech@gmail.com>
2024-03-09 19:56:09 +0200

    Implement filtering by username

--- UsersWindow.xaml.cs
+++ UsersWindow.xaml.cs
@@ -1,14 +1,14 @@
-﻿using Microsoft.VisualBasic.Logging;
-using System.Data;
-using System.Data.Common;
+﻿using System.Data;
 using System.Data.SqlClient;
+using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Input;
 
 namespace TodoApp
 {
     public partial class UsersWindow : UserControl
     {
+        private SqlConnection _dbConnection;
+
         private SqlDataAdapter _dataAdapter;
 
         private DataSet _dataSet;
@@ -20,20 +20,23 @@ namespace TodoApp
 
         private void LoadUsers()
         {
-            SqlConnection dbConnection = new SqlConnection(DatabaseConnection.ConnenctionString);
-            _dataAdapter = new SqlDataAdapter("SELECT * FROM users", dbConnection);
-            SqlCommand updateCommand = new SqlCommand("UPDATE users SET password=@password WHERE id=@id", dbConnection);
+            _dbConnection = new SqlConnection(DatabaseConnection.ConnenctionString);
+            _dataAdapter = new SqlDataAdapter();
+            _dataAdapter.SelectCommand = new SqlCommand("SELECT * FROM users WHERE username LIKE @username", _dbConnection);
+            _dataAdapter.SelectCommand.Parameters.AddWithValue("@username", $"%{searchBox.Text}%");
+
+            SqlCommand updateCommand = new SqlCommand("UPDATE users SET password=@password WHERE id=@id", _dbConnection);
             updateCommand.Parameters.Add("@username", SqlDbType.NVarChar, 255, "username");
             updateCommand.Parameters.Add("@password", SqlDbType.Int, 5, "password");
             updateCommand.Parameters.Add("@id", SqlDbType.Int, 5, "id");
             _dataAdapter.UpdateCommand = updateCommand;
 
-            SqlCommand insertCommand = new SqlCommand("INSERT INTO users (username, password) VALUES (@username, @password)", dbConnection);
+            SqlCommand insertCommand = new SqlCommand("INSERT INTO users (username, password) VALUES (@username, @password)", _dbConnection);
             insertCommand.Parameters.Add("@username", SqlDbType.NVarChar, 255, "username");
             insertCommand.Parameters.Add("@password", SqlDbType.NVarChar, 255, "password");
             _dataAdapter.InsertCommand = insertCommand;
 
-            SqlCommand deleteCommand = new SqlCommand("DELETE FROM users WHERE id=@id", dbConnection);
+            SqlCommand deleteCommand = new SqlCommand("DELETE FROM users WHERE id=@id", _dbConnection);
             deleteCommand.Parameters.Add("@id", SqlDbType.Int, 5, "id");
             _dataAdapter.DeleteCommand = deleteCommand;
 
@@ -42,14 +45,31 @@ namespace TodoApp
             UsersGrid.ItemsSource = _dataSet.Tables[0].DefaultView;
         }
 
-        private void WindowLoaded(object sender, System.Windows.RoutedEventArgs e)
+        private void WindowLoaded(object sender, RoutedEventArgs e)
         {
             LoadUsers();
         }
 
-        private void SaveButton_Click(object sender, System.Windows.RoutedEventArgs e)
+        private void SaveButton_Click(object sender, RoutedEventArgs e)
         {
             _dataAdapter.Update(_dataSet);
+        }
+
+        private void SearchButton_Click(object sender, RoutedEventArgs e)
+        {
+            FilterUserByUsernameFromDataView();
+        }
+
+        private void FilterUserByUsernameFromDataView()
+        {
+            _dataSet.Tables[0].DefaultView.RowFilter = $"username LIKE '%{searchBox.Text}%'";
+        }
+
+        private void FilterUserByUsernameFromDb()
+        {
+            _dataAdapter.SelectCommand.Parameters["@username"].Value = $"%{searchBox.Text}%";
+            _dataSet.Clear();
+            _dataAdapter.Fill(_dataSet);
         }
     }
 
